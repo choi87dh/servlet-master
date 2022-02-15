993aa8b76f69b9559f0223f7dc3e1b7b2aad2855
Admin <choi87dh@naver.com>
2022-02-15 11:16:48 +0900

    2022-02-15 commit

---
+++ src/main/java/hello/servlet/web/servlet/springmvc/old/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.servlet.springmvc.old.v1;
+
+import hello.servlet.web.servlet.frontController.v1.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process(){
+        return new ModelAndView("new-form");
+    }
+}
